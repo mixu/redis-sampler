b3ea35f50e6a218739b71f397013aaa4a325bce0
Mikito Takada <mikito.takada@gmail.com>
2013-03-14 14:23:28 -0700

    Add ability to look at keys by prefix

--- redis-sampler.rb
+++ redis-sampler.rb
@@ -1,11 +1,11 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 # Copyright (c) 2011, Salvatore Sanfilippo
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -14,7 +14,7 @@
 #     * Neither the name of Redis nor the names of its contributors may be
 #       used to endorse or promote products derived from this software without
 #       specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 # TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -48,9 +48,12 @@ class RedisSampler
         @string_elesize = {}
     end
 
-    def incr_freq_table(hash,item)
-        hash[item] = 0 if !hash[item]
-        hash[item] += 1
+    def incr_freq_table(hash, prefix, item)
+        if !hash[prefix]
+            hash[prefix] = {}
+        end
+        hash[prefix][item] = 0 if !hash[prefix][item]
+        hash[prefix][item] += 1
     end
 
     def sample
@@ -60,11 +63,13 @@ class RedisSampler
                 @redis.type(k)
                 @redis.ttl(k)
             }
+            # from beginning to first ":"
+            prefix = k.split(':', 2)[0]
             t = p[0]
             x = p[1]
             x = 'unknown' if x.to_i == -1
-            incr_freq_table(@types,t)
-            incr_freq_table(@expires,x)
+            incr_freq_table(@types, prefix, t)
+            incr_freq_table(@expires, prefix, x)
             case t
             when 'zset'
                 p = @redis.pipelined {
@@ -73,8 +78,8 @@ class RedisSampler
                 }
                 card = p[0]
                 ele = p[1][0]
-                incr_freq_table(@zset_card,card) if card != 0
-                incr_freq_table(@zset_elesize,ele.length) if ele
+                incr_freq_table(@zset_card, prefix, card) if card != 0
+                incr_freq_table(@zset_elesize, prefix, ele.length) if ele
             when 'set'
                 p = @redis.pipelined {
                     @redis.scard(k)
@@ -82,8 +87,8 @@ class RedisSampler
                 }
                 card = p[0]
                 ele = p[1]
-                incr_freq_table(@set_card,card) if card != 0
-                incr_freq_table(@set_elesize,ele.length) if ele
+                incr_freq_table(@set_card, prefix, card) if card != 0
+                incr_freq_table(@set_elesize, prefix, ele.length) if ele
             when 'list'
                 p = @redis.pipelined {
                     @redis.llen(k)
@@ -91,24 +96,24 @@ class RedisSampler
                 }
                 len = p[0]
                 ele = p[1][0]
-                incr_freq_table(@list_len,len) if len != 0
-                incr_freq_table(@list_elesize,ele.length) if ele
+                incr_freq_table(@list_len, prefix, len) if len != 0
+                incr_freq_table(@list_elesize, prefix, ele.length) if ele
             when 'hash'
                 l = @redis.hlen(k)
-                incr_freq_table(@hash_len,l) if l != 0
-                if l >= 32
+                incr_freq_table(@hash_len, prefix, l) if l != 0
+                if l >= 1
                     field = @redis.hkeys(k)[0]
                     if field
-                        incr_freq_table(@hash_fsize,field.length)
+                        incr_freq_table(@hash_fsize, prefix, field.length)
                         val = @redis.hget(k,field)
-                        incr_freq_table(@hash_vsize,val.length) if val
+                        incr_freq_table(@hash_vsize, prefix, val.length) if val
                     end
                 else
-                    incr_freq_table(@hash_fsize,'unknown')
-                    incr_freq_table(@hash_vsize,'unknown')
+                    incr_freq_table(@hash_fsize, prefix, 'unknown')
+                    incr_freq_table(@hash_vsize, prefix, 'unknown')
                 end
             when 'string'
-                incr_freq_table(@string_elesize,@redis.strlen(k))
+                incr_freq_table(@string_elesize, prefix, @redis.strlen(k))
             end
         }
     end
@@ -117,12 +122,20 @@ class RedisSampler
         sprintf("%.2f%%",val*100/tot.to_f)
     end
 
-    def render_freq_table(title,hash)
+    def render_each_freq_table(title,hash)
         puts "\n#{title.upcase}\n#{"="*title.length}\n" if title
+
+        hash.each{|prefix, h|
+            puts "\n# for #{prefix}"
+            render_freq_table(nil, h)
+        }
+   end
+
+    def render_freq_table(title,hash)
         h = hash.sort{|a,b| b[1]<=>a[1]}
         i = 0
         tot = 0
-        h.each{|k,v| tot += v}
+        h.each{|k,v| tot += v.to_i}
         partial = 0
         h.each{|k,v|
             partial += v
@@ -148,15 +161,18 @@ class RedisSampler
     end
 
     def render_power_table(hash)
-        puts "\nPowers of two distribution: (NOTE <= p means: p/2 < x <= p)"
-        pow = {}
-        hash.each{|k,v|
-            next if k == 'unknown'
-            p = "<= #{num_to_pow(k)}"
-            pow[p] = 0 if !pow[p]
-            pow[p] += v
+        hash.each{|prefix, hash|
+            puts "\n# for #{prefix}"
+            puts "\nPowers of two distribution: (NOTE <= p means: p/2 < x <= p)"
+            pow = {}
+            hash.each{|k,v|
+                next if k == 'unknown'
+                p = "<= #{num_to_pow(k)}"
+                pow[p] = 0 if !pow[p]
+                pow[p] += v
+            }
+            render_freq_table(nil,pow)
         }
-        render_freq_table(nil,pow)
     end
 
     def compute_avg(hash)
@@ -186,55 +202,58 @@ class RedisSampler
     end
 
     def render_avg(hash)
-        data = compute_avg(hash)
-        printf " Average: %.2f Standard Deviation: %.2f",data[:avg],data[:stddev]
-        puts ""
-        puts " Min: #{data[:min]} Max: #{data[:max]}"
+        hash.each{|prefix, hash|
+            puts "\n# for #{prefix}"
+            data = compute_avg(hash)
+            printf " Average: %.2f Standard Deviation: %.2f",data[:avg],data[:stddev]
+            puts ""
+            puts " Min: #{data[:min]} Max: #{data[:max]}"
+        }
     end
 
     def stats
-        render_freq_table("Types",@types)
-        render_freq_table("Expires",@expires)
+        render_each_freq_table("Types",@types)
+        render_each_freq_table("Expires",@expires)
         render_avg(@expires)
         render_power_table(@expires)
         puts "\nNote: 'unknown' expire means keys with no expire"
         if @string_elesize.length != 0
-            render_freq_table("Strings, size of values",@string_elesize)
+            render_each_freq_table("Strings, size of values",@string_elesize)
             render_avg(@string_elesize)
             render_power_table(@string_elesize)
         end
         if @list_len.length != 0
-            render_freq_table("Lists, number of elements",@list_len)
+            render_each_freq_table("Lists, number of elements",@list_len)
             render_avg(@list_len)
             render_power_table(@list_len)
-            render_freq_table("Lists, size of elements",@list_elesize)
+            render_each_freq_table("Lists, size of elements",@list_elesize)
             render_avg(@list_elesize)
             render_power_table(@list_elesize)
         end
         if @set_card.length != 0
-            render_freq_table("Sets, number of elements",@zset_card)
+            render_each_freq_table("Sets, number of elements",@zset_card)
             render_avg(@set_card)
             render_power_table(@set_card)
-            render_freq_table("Sets, size of elements",@set_elesize)
+            render_each_freq_table("Sets, size of elements",@set_elesize)
             render_avg(@set_elesize)
             render_power_table(@set_elesize)
         end
         if @zset_card.length != 0
-            render_freq_table("Sorted sets, number of elements",@zset_card)
+            render_each_freq_table("Sorted sets, number of elements",@zset_card)
             render_avg(@zset_card)
             render_power_table(@zset_card)
-            render_freq_table("Sorted sets, size of elements",@zset_elesize)
+            render_each_freq_table("Sorted sets, size of elements",@zset_elesize)
             render_avg(@zset_elesize)
             render_power_table(@zset_elesize)
         end
         if @hash_len.length != 0
-            render_freq_table("Hashes, number of fields",@hash_len)
+            render_each_freq_table("Hashes, number of fields",@hash_len)
             render_avg(@hash_len)
             render_power_table(@hash_len)
-            render_freq_table("Hashes, size of fields",@hash_fsize)
+            render_each_freq_table("Hashes, size of fields",@hash_fsize)
             render_avg(@hash_fsize)
             render_power_table(@hash_fsize)
-            render_freq_table("Hashes, size of values",@hash_vsize)
+            render_each_freq_table("Hashes, size of values",@hash_vsize)
             render_avg(@hash_vsize)
             render_power_table(@hash_vsize)
         end
